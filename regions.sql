15819f747a7e26d94195d516d9f0bb32fa60fd2a
brad harmon <bradharmon@gmail.com>
2011-06-09 00:50:03 -0500

    added page name to table. possible to have the same div on different pages

--- regions.sql
+++ regions.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `regions` (
   `id` int(4) unsigned NOT NULL auto_increment,
+  `page` varchar(255) NOT NULL,
   `div_id` varchar(32) NOT NULL,
-  `data` varchar(50000) NOT NULL,
+  `content` varchar(50000) NOT NULL,
   PRIMARY KEY  (`id`)
 );
