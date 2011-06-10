f183950ba6eacb2ad042d93680318338522efbf7
brad harmon <bradharmon@gmail.com>
2011-06-10 17:53:04 -0500

    add php dom parser and do some more reorganizing

--- includes/sqlStrings.php
+++ includes/sqlStrings.php
@@ -1,10 +1,10 @@
 <?php
 //database connect
 
-define ('DB_USER', 'db user name');
-define ('DB_PASSWORD', 'db password');
+define ('DB_USER', 'hsproduc_cms');
+define ('DB_PASSWORD', 'cms');
 define ('DB_HOST', 'localhost');
-define ('DB_NAME', 'db name');
+define ('DB_NAME', 'hsproduc_cms');
 
 $dbc = mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) OR die('Could not connect to MySQL: ' . mysql_error());
 mysql_select_db (DB_NAME) OR die('Could not select the database: ' . mysql_error());
