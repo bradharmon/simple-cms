e1876bd01758e1a86865965641291ba441e1365f
brad harmon <bradharmon@gmail.com>
2011-06-12 21:37:56 -0500

    add datetime field to the database for historical purposes

--- setup/regions.sql
+++ setup/regions.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `regions` (
   `id` int(4) unsigned NOT NULL auto_increment,
+  `date` datetime NOT NULL,
   `page` varchar(255) NOT NULL,
   `div_id` varchar(32) NOT NULL,
   `content` varchar(50000) NOT NULL,
