38f9835615447891edada818e4e80e7cbacc35d3
brad harmon <bradharmon@gmail.com>
2011-06-12 21:34:30 -0500

    sort by date instead of id

--- php/getRegionFromDB.php
+++ php/getRegionFromDB.php
@@ -4,7 +4,7 @@ function get_region_from_db($page, $id)
    $page = mysql_real_escape_string($page);
    $id = mysql_real_escape_string($id);
 
-   $query = "SELECT content FROM regions WHERE page = \"$page\" AND div_id = \"$id\" ORDER by id DESC LIMIT 1";
+   $query = "SELECT content FROM regions WHERE page = \"$page\" AND div_id = \"$id\" ORDER by date DESC LIMIT 1";
    $result = mysql_query($query);
    $row = mysql_fetch_row($result);
    echo stripslashes($row[0]);
