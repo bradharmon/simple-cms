c9a11eb377e922da4c872f9e6ecefb12a791ff3c
brad harmon <bradharmon@gmail.com>
2011-06-19 14:36:13 -0500

    add div wrapper for editor around content

--- php/getRegionFromDB.php
+++ php/getRegionFromDB.php
@@ -7,6 +7,8 @@ function get_region_from_db($page, $id)
    $query = "SELECT content FROM regions WHERE page = \"$page\" AND div_id = \"$id\" ORDER by date DESC LIMIT 1";
    $result = mysql_query($query);
    $row = mysql_fetch_row($result);
+   echo "<div class=\"editor\">\n";
    echo stripslashes($row[0]);
+   echo "</div>";
 }
 ?>
