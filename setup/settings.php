06e8a3f18586c85ea0678c55d1885f00e45d4ddc
brad harmon <bradharmon@gmail.com>
2011-07-06 14:47:39 -0500

    move setup/settings.php to simple-cms/settings.php

--- setup/settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-$websiteBaseDir = '/home/hsproduc/public_html/simple-cms';
-$simpleCmsBaseDir = '/home/hsproduc/public_html/simple-cms';
-$websiteBaseUrl = 'http://hs-productions.com/simple-cms';
-?>
